b1e49b6f9bfa02787eced8eea5ab352d14cf4646
Alex Miller <alex.k.miller@gmail.com>
2018-10-17 10:36:28 -0400

    Final master file

--- project_code/master.R
+++ project_code/master.R
@@ -1,14 +1,14 @@
 # Set Working directory
-setwd("~/git/domestic_finance_2018")
+setwd("/home/alex/git/domestic_finance_2018")
 
 # Run Python script
 system("python3 project_code/levels.py")
 
 # Run R scripts
-source("project_code/imf_deflator.R")
-source("project_code/calculate_conversion_factor.R")
-source("project_code/levels.R")
-source("project_code/govrev.R")
-source("project_code/grants-pct.R")
-source("project_code/non-grant.R")
-source("project_code/total-expend.R")
+source("/home/alex/git/domestic_finance_2018/project_code/imf_deflator.R");rm(list=ls());
+source("/home/alex/git/domestic_finance_2018/project_code/calculate_conversion_factor.R");rm(list=ls());
+source("/home/alex/git/domestic_finance_2018/project_code/levels.R");rm(list=ls());
+source("/home/alex/git/domestic_finance_2018/project_code/govrev.R");rm(list=ls());
+source("/home/alex/git/domestic_finance_2018/project_code/grants-pct.R");rm(list=ls());
+source("/home/alex/git/domestic_finance_2018/project_code/non-grant.R");rm(list=ls());
+source("/home/alex/git/domestic_finance_2018/project_code/total-expend.R");rm(list=ls())
